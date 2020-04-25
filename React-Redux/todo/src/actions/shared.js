8a347e1cf9d4300910ee728736339130946f4c06
hyejshin <hyejungshin7@gmail.com>
2020-04-25 16:57:23 -0700

    Rails-style folder structure

---
+++ React-Redux/todo/src/actions/shared.js
@@ -0,0 +1,23 @@
+import API from 'goals-todos-api'
+
+export const RECEIVE_DATA = 'RECEIVE_DATA'
+
+function receiveDataAction (todos, goals) {
+    return {
+        type: RECEIVE_DATA,
+        todos,
+        goals,
+    }
+}
+
+export function handleInitialData () {
+    return (dispatch) => {
+        Promise.all([
+            API.fetchTodos(),
+            API.fetchGoals()
+        ]).then(([ todos, goals ]) => {
+            dispatch(receiveDataAction(todos, goals))
+        })
+
+    }
+}
