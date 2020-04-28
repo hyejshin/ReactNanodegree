a72b62dfd7f0779e61ca70522a7eac9ea1ba5d4d
hyejshin <hyejungshin7@gmail.com>
2020-04-28 00:32:30 -0700

    Add Dashboard component

--- React-Redux/reactnd-chirper-app-master/src/components/App.js
+++ React-Redux/reactnd-chirper-app-master/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
 
 class App extends Component {
   componentDidMount() {
@@ -9,9 +10,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        {this.props.loading === true
+          ? null
+        : <Dashboard /> }
       </div>
     )
+  }
+}
+
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
   }
 }
 
