c236ccf4eba12b76c3adbaeccc152b6ae35ebf9c
hyejshin <hyejungshin7@gmail.com>
2020-05-16 14:26:14 -0700

    Touchables button examples

--- React-Native/UdaciFitness/App.js
+++ React-Native/UdaciFitness/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { 
   View,
+  Text,
+  StyleSheet,
   TouchableHighlight,
   TouchableNativeFeedback,
   TouchableOpacity,
@@ -9,18 +11,54 @@ import {
 import AddEntry from './components/AddEntry'
 
 export default class App extends Component {
-  componentDidMount () {
-    console.log('Before')
-    debugger
-    console.log('After')
+  handlePress = () => {
+    alert('Hello!')
   }
   render() {
     return (
-      <View>
-        <TouchableHighlight>
-          
+      <View style={styles.container}>
+        <TouchableHighlight style={styles.btn} onPress={this.handlePress} underlayColor='#d4271b'>
+          <Text style={styles.btnText}>Touchable Highlight</Text>
         </TouchableHighlight>
+
+        <TouchableOpacity style={styles.btn} onPress={this.handlePress}>
+          <Text style={styles.btnText}>Touchable Opacity</Text>
+        </TouchableOpacity>
+
+        <TouchableWithoutFeedback onPress={this.handlePress}>
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>Touchable Without Feedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+        <TouchableNativeFeedback background={TouchableNativeFeedback.SelectableBackground()}>
+            <View style={styles.btn}>
+              <Text style={styles.btnText}>Touchable Native Feedback</Text>
+            </View>
+        </TouchableNativeFeedback>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: '#E53224',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  btnText: {
+    color: '#fff'
+  }
+});
