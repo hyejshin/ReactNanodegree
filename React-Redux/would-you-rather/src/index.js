d5cd672f635a4a5abb262fc39e4b74f9569ec2c3
hyejshin <hyejungshin7@gmail.com>
2020-08-06 01:21:52 -0700

    Add persistedState and local storage

--- React-Redux/would-you-rather/src/index.js
+++ React-Redux/would-you-rather/src/index.js
@@ -8,7 +8,33 @@ import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
 
-const store = createStore(reducer, middleware)
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if(serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    console.log(e)
+  }
+};
+
+const persistedState = loadFromLocalStorage()
+
+const store = createStore(reducer, persistedState, middleware)
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
+});
 
 ReactDOM.render(
   <Provider store={store}>
